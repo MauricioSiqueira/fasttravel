0bfc4b746c31fbf224e74261d20770897b58edf5
Guilherme D. Possas <possasg@gmail.com>
2024-06-12 14:49:19 -0300

    feat: add sign_in operation

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,15 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { createUserDTO } from './dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { SkipAuth } from './decorators/skip_auth.decorator';
+import { AuthUserDTO } from './dto/auth-user.dto';
 
 @Controller('/auth')
 export class AuthController
 {
   constructor( private readonly auth_service: AuthService ){}
 
+  @SkipAuth()
   @Post( '/sign_up' )
   sign_up( @Body() create_user_dto: createUserDTO )
   {
     return this.auth_service.create_user( create_user_dto );
+  }
+
+  @SkipAuth()
+  @Post( '/sign_in' )
+  sign_in( @Body() auth_user_dto: AuthUserDTO )
+  {
+    return this.auth_service.validate_user( auth_user_dto );
   }
 }
--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { createUserDTO } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { hash } from 'bcrypt';
+import { compare, hash } from 'bcrypt';
+import { AuthUserDTO } from './dto/auth-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayloadInterface } from './entities/jwt-payload.interface';
 
 @Injectable()
 export class AuthService
 {
-  constructor( private readonly prisma: PrismaService ){}
+  constructor( private readonly prisma: PrismaService, private readonly jwt_service: JwtService ){}
 
   async create_user( create_user_dto: createUserDTO )
   {
@@ -24,4 +27,25 @@ export class AuthService
 
     return user;
   }
+
+  async validate_user( auth_user_dto: AuthUserDTO )
+  {
+    const { email, password } = auth_user_dto;
+
+    const user = await this.prisma.user.findUnique( { where: { email: email } } );
+
+    if ( user && await compare( password, user.password ) )
+    {
+      const payload: JWTPayloadInterface = { sub: user.id, email: email, name: user.name };
+      return { 
+        user_id: user.id, 
+        name: user.name, 
+        access_token: await this.jwt_service.signAsync( payload ) 
+      };
+    }
+    
+    throw new UnauthorizedException('Invalid credentials');
+  }
+
+  
 }
