33ae392bde937da7cf0f72427403320b9ca2deaa
Guilherme D. Possas <possasg@gmail.com>
2024-06-12 17:02:53 -0300

    feat: import travel module

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { TravelsModule } from './travels/travels.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, AuthModule, TravelsModule],
   controllers: [],
   providers: [],
 })
