890915cd0e4adb7a53831843f5d3079921a68be7
Guilherme D. Possas <possasg@gmail.com>
2024-06-12 14:51:01 -0300

    feat: import jwt module and provide guard

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [PrismaModule],
-  providers: [AuthService],
+  imports: [PrismaModule, 
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '60s' },
+    })
+  ],
+  providers: [AuthService, { provide: APP_GUARD, useClass: AuthGuard }],
   controllers: [AuthController]
 })
 export class AuthModule {}
